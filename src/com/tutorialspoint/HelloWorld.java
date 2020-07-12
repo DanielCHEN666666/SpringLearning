4b5c5c6bdcb485336e2fc5ef216cce345e9c8bd8
ZEMIN/CHEN <u6293777@anu.edu.au>
2020-07-12 13:13:34 +0800

    beanlifecircle

--- src/com/tutorialspoint/HelloWorld.java
+++ src/com/tutorialspoint/HelloWorld.java
@@ -2,8 +2,8 @@ package com.tutorialspoint;
 
 public class HelloWorld {
 
+    // 和Beans.xml里的 property name 命名要一致
     private String message;
-
 
     public void setMessage(String message){
         this.message  = message;
@@ -11,5 +11,12 @@ public class HelloWorld {
 
     public void getMessage(){
         System.out.println("Your Message : " + message);
+    }
+
+    public void init(){
+        System.out.println("Bean is going through init.");
+    }
+    public void destroy(){
+        System.out.println("Bean will destroy now.");
     }
 }
--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,6 +1,9 @@
 package com.tutorialspoint;
+
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Spring BeanFactory是一个最简单的容器，它主要的功能是为依赖注入 （DI） 提供支持，
@@ -12,23 +15,45 @@ public class MainApp {
     public static void main(String[] args) {
 
         /*
-        第一步利用框架提供的 XmlBeanFactory() API 去生成工厂 bean
-        以及利用 ClassPathResource() API 去加载在路径 CLASSPATH 下可用的 bean 配置文件。
-        XmlBeanFactory() API 负责创建并初始化所有的对象，即在配置文件中提到的 bean。
+         * 第一步利用框架提供的 XmlBeanFactory() API 去生成工厂 bean
+         * 以及利用 ClassPathResource() API 去加载在路径 CLASSPATH 下可用的 bean 配置文件。
+         * XmlBeanFactory() API 负责创建并初始化所有的对象，即在配置文件中提到的 bean。
          */
-        /* 在 Spring 中，有大量对 BeanFactory 接口的实现。
-         其中，最常被使用的是 XmlBeanFactory 类。
-         这个容器从一个 XML 文件中读取配置元数据，由这些元数据来生成一个被配置化的系统或者应用。
-        */
-        XmlBeanFactory factory = new XmlBeanFactory
-                (new ClassPathResource("Beans.xml"));
+        /*
+         * 在 Spring 中，有大量对 BeanFactory 接口的实现。
+         * 其中，最常被使用的是 XmlBeanFactory 类。
+         * 这个容器从一个 XML 文件中读取配置元数据，由这些元数据来生成一个被配置化的系统或者应用。
+         */
+
+//        XmlBeanFactory factory = new XmlBeanFactory
+//                (new ClassPathResource("Beans.xml"));
+
+
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
         /*
-        第二步利用第一步生成的 bean 工厂对象的 getBean() 方法得到所需要的 bean。
-        这个方法通过配置文件中的 bean ID 来返回一个真正的对象，该对象最后可以用于实际的对象。
-        一旦得到这个对象，你就可以利用这个对象来调用任何方法。
+         * 第二步利用第一步生成的 bean 工厂对象的 getBean() 方法得到所需要的 bean。
+         * 这个方法通过配置文件中的 bean ID 来返回一个真正的对象，该对象最后可以用于实际的对象。
+         * 一旦得到这个对象，你就可以利用这个对象来调用任何方法。
+         *
          */
-        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
         obj.getMessage();
+
+        // set message, and the message would be changed
+        obj.setMessage("Changed World!");
+        // return: Changed World!
+        obj.getMessage();
+
+        /*
+         * 在JVM中注册关闭 hook。这样做可以确保正常关闭，为了让所有的资源都被释放，可以在单个 beans 上调用 destroy 方法。
+         *
+         * 实现上需要注册一个在 AbstractApplicationContext 类中声明的关闭 hook 的 registerShutdownHook() 方法。
+         * 它将确保正常关闭，并且调用相关的 destroy 方法。
+         *
+         * 一般需要销毁时添加
+         */
+        context.registerShutdownHook();
     }
 }
